a52143ab2d11ce63d6f0cd29493609efce839677
Niall Fraser <niallfraser92@gmail.com>
2019-01-16 15:39:15 -0600

    added arrays and objects

---
+++ src/journal.js
@@ -0,0 +1,21 @@
+const allDailyEntries = []
+
+const objectsJournalEntry = [
+    {
+        Date: 1 / 15 / 2019,
+        Entry: "Today was all about building objects and arrays",
+        Mood: "OK"
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    }
+]
+
+function allJournalEntries() {
+    allDailyEntries.push(objectsJournalEntry)
+}
+
+allJournalEntries()
