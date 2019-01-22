1c94ba8a4da854e52d6fad6bc99630a993200853
Niall Fraser <niallfraser92@gmail.com>
2019-01-22 15:13:54 -0600

    unsaved files were pushed. repushing

---
+++ src/entries.js
@@ -0,0 +1,79 @@
+let allJournalEntries = [
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 15 / 2019,
+        Entry: "Today was all about building objects and arrays",
+        Mood: "OK"
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+    {
+        Date: 1 / 14 / 2019,
+        Entry: "We learned about accessing objects and arrays",
+        Mood: "OK"
+
+    },
+]
--- src/journal.js
+++ src/journal.js
@@ -1,25 +1,3 @@
-let allDailyEntries = []
-
-let objectsJournalEntry = [
-    {
-        Date: 1 / 15 / 2019,
-        Entry: "Today was all about building objects and arrays",
-        Mood: "OK"
-    },
-    {
-        Date: 1 / 14 / 2019,
-        Entry: "We learned about accessing objects and arrays",
-        Mood: "OK"
-
-    }
-]
-
-function allJournalEntries() {
-    allDailyEntries.push(objectsJournalEntry)
-}
-
-allJournalEntries()
-
 /*
     Purpose: To create, and return, a string template that
     represents a single journal entry object as HTML
