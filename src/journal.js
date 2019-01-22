65086305c7dd80e9046c8c75b62fe2aad907536a
Niall Fraser <niallfraser92@gmail.com>
2019-01-22 15:11:39 -0600

    added new JavaScript file to keep entries object

--- src/journal.js
+++ src/journal.js
@@ -1,6 +1,6 @@
-const allDailyEntries = []
+let allDailyEntries = []
 
-const objectsJournalEntry = [
+let objectsJournalEntry = [
     {
         Date: 1 / 15 / 2019,
         Entry: "Today was all about building objects and arrays",
@@ -19,3 +19,35 @@ function allJournalEntries() {
 }
 
 allJournalEntries()
+
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+
+    Arguments: journalEntry (object)
+*/
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+    section class="journal_section"
+    <h3>${journalDate}</h3>
+    <h3>${conceptsCovered}</h3>
+    <p>${journalEntry}</p>
+    <p>${moodForDay}</p>
+    </section>
+    `
+}
+
+const journalSection = document.querySelector("#journal_section")
+
+// Create HTML structure and add to DOM
+for (let entry of objectsJournalEntry) {
+    let newEntry = makeJournalEntryComponent(entry)
+    journalSection.innerHTML += newEntry
+}
+
+const renderJournalEntries = (entry) => {
+    const journalEl = document.querySelector("#journalSection")
+}
+
+// Invoke the render function
+renderJournalEntries(entry)
